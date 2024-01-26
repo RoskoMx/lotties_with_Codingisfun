75ca966a41ed29a983cfdde6517751f949574383
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-01-26 08:04:04 -0600

    Update lotties.py

--- lotties.py
+++ lotties.py
@@ -20,7 +20,7 @@ def load_lottieurl(url: str):
     return r.json()
     
 
-lottie_coding = load_lottiefile("Animation - 1706015468852.json")  # replace link to local lottie file
+#lottie_coding = load_lottiefile("Animation - 1706015468852.json")  # replace link to local lottie file
 lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_M9p23l.json")
 
 st_lottie(
